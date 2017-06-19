e6715a607d22b38159171dd4bfe3941fddbf454c
Ciunas <ciunas.bennett@gmail.com>
2017-06-19 15:31:51 +0100

    update

--- Server/Server.ino
+++ Server/Server.ino
@@ -1,34 +1,14 @@
 /*
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
  Circuit:
  * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
-
  */
+ 
 #include <UIPEthernet.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+// Enter a MAC address and IP address 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,10,2);
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
+EthernetServer server(80); // (port 80 is default for HTTP):
 
 void setup() {
   // Open serial communications and wait for port to open:
